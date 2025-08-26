f43736a6df01d9b4408fd1329aee50459e6af4b8
Ricardo Mello <ricardohsmello@gmail.com>
2025-08-26 18:51:25 -0300

    feat: implement MongoDB Hibernate POC - configure Hibernate with MongoDB dialect - add Book entity with ObjectId generator - create Books CLI menu (create, list, find by title, delete)

--- src/main/java/com/mongodb/HibernateUtil.java
+++ src/main/java/com/mongodb/HibernateUtil.java
@@ -1,35 +1,13 @@
 package com.mongodb;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-import java.util.Properties;
+import org.hibernate.cfg.Configuration;
 
 public final class HibernateUtil {
-  private static final SessionFactory SESSION_FACTORY;
-
-  static {
-    Properties p = new Properties();
-    p.put("hibernate.dialect", "com.mongodb.hibernate.dialect.MongoDialect");
-    p.put("hibernate.connection.provider_class", "com.mongodb.hibernate.jdbc.MongoConnectionProvider");
-    p.put("jakarta.persistence.jdbc.url", "mongodb://localhost:27017/mydb?replicaSet=rs0");
-    p.put("hibernate.hbm2ddl.auto", "none");
-    p.put("hibernate.show_sql", "true");
-
-    StandardServiceRegistry registry =
-        new StandardServiceRegistryBuilder().applySettings(p).build();
-    Metadata metadata = new MetadataSources(registry)
-        .addAnnotatedClass(com.mongodb.Book.class)
-        .buildMetadata();
-    SESSION_FACTORY = metadata.buildSessionFactory();
-  }
+  private static final SessionFactory SESSION_FACTORY =
+          new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 
   private HibernateUtil() {}
 
-  public static SessionFactory getSessionFactory() {
-    return SESSION_FACTORY;
-  }
+  public static SessionFactory getSessionFactory() { return SESSION_FACTORY; }
 }
